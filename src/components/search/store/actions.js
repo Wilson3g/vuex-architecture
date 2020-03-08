87bb7eb264313cd44ef710fe64ec65983bc25685
wilson3g <wilson.sfj@outlook.com>
2020-03-08 14:34:33 -0300

    get users repositories

--- src/components/search/store/actions.js
+++ src/components/search/store/actions.js
@@ -2,7 +2,7 @@ import * as types from './mutation-types'
 import {getUsersData} from '@/services'
 
 export const ActionsSearch = ({ commit } ,payload) => {
-    return getUsersData(payload) .then(resp => {
-        context.commit(types.SET_SEARCH, resp)
+    return getUsersData(payload).then(resp => {
+        commit(types.SET_SEARCH, resp)
     })
 }
--- src/services/index.js
+++ src/services/index.js
@@ -4,5 +4,5 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource)
 
 export const getUsersData = (user) => {
-    return Vue.http.get(`https://api.github.com/users/${user}`);
+    return Vue.http.get(`https://api.github.com/users/${user}/repos`);
 }
